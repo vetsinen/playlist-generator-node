a605d7f303aa0cbedda265aee144413911f15582
vetsinen <vetsinen@gmail.com>
2021-02-09 20:39:09 +0200

    fixed file path

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const startDir = '/home/jsdev/bachasalsa/'
 const mc: MusicCollection = new MusicCollection('/home/jsdev/bachasalsa/')
 
 let pl1 = mc.genNBlocksPlaylist(5, 's2b2')
-mc.exportPlaylistAsM3u(pl1,'/home/jsdev/bachasalsa/','/home/jsdev/Music/')
+mc.exportPlaylistAsM3u(pl1,'/home/jsdev/bachasalsa/')
 // mc.exportPlaylistAsM3u(['bacha/bacha-1-chiquilla chiquita.mp3','bacha/bacha-1-v5 - bacha-1-calma.mp3'],'bachasalsa')
 // print(mc.getRandomTrackBySubgenre('bacha'))
 // print(mc.block2s2b())
